80081c6c4fe9267ffe0acabd483d409e91f250a8
fattaneh Ameri Mahabadian <fatameri@hotmail.com>
2018-05-28 00:41:06 -0400

    My first intelli

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,2 +1,8 @@
 public class Arrays {
+    int[] a = new int[5];
+    double[] b = new double[10];
+    char[] c = new char[20];
+    int aLength = a.length; // returns 5
+    int bLength = b.length; // returns 10
+    int cLength = c.length; // returns 20
 }
