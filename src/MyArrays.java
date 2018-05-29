cbe5effdb022f3d415bcd4a136511a9e4c6e7a4b
fattaneh Ameri Mahabadian <fatameri@hotmail.com>
2018-05-28 20:37:52 -0400

    My first intelli

--- src/MyArrays.java
+++ src/MyArrays.java
@@ -1,8 +1,18 @@
-public class Arrays {
+import java.util.Arrays;
+
+public class MyArrays {
     int[] a = new int[5];
     double[] b = new double[10];
     char[] c = new char[20];
     int aLength = a.length; // returns 5
     int bLength = b.length; // returns 10
     int cLength = c.length; // returns 20
+
+    public static void twoDArray(){
+
+
+        char[][] arr = new char[5][5];
+        System.out.print(Arrays.toString(arr));
+    }
 }
+
