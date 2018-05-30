ebcc840c547b3cdd971e3c4828a0e7a6fc96fee9
fattaneh Ameri Mahabadian <fatameri@hotmail.com>
2018-05-30 09:15:44 -0400

    My first intelli

---
+++ src/AbstractClass.java
@@ -0,0 +1,15 @@
+public class AbstractClass {
+
+    public abstract class Animal {
+
+
+        abstract boolean consumeFood();
+
+
+        public Distance move() {
+            return new Distance(1);
+        }
+
+        abstract Animal produceOffspring();
+    }
+}
