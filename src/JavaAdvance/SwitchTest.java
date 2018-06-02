6cb61a765ce5a985ea56d0fb2b4c1480a78f4604
fattaneh Ameri Mahabadian <fatameri@hotmail.com>
2018-06-02 11:11:45 -0400

    My first intelli

---
+++ src/JavaAdvance/SwitchTest.java
@@ -0,0 +1,22 @@
+package JavaAdvance;
+
+
+
+public class SwitchTest {
+
+    public static void main(String[] args){
+
+        String key="One";
+
+        switch (key) {
+            case "One":
+System.out.print("The value is one");
+                break;
+
+            default:
+                System.out.print("The value is not one");
+                break;
+        }
+
+    }
+}
