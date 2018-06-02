715168c1d357f19b36b6d171aa6a932f0bc52675
fattaneh Ameri Mahabadian <fatameri@hotmail.com>
2018-06-02 11:30:57 -0400

    My first intelli

--- src/JavaAdvance/SwitchTest.java
+++ src/JavaAdvance/SwitchTest.java
@@ -1,6 +1,8 @@
 package JavaAdvance;
 
+import com.lynda.olivepress.olives.Olive;
 
+import java.util.ArrayList;
 
 public class SwitchTest {
 
@@ -17,6 +19,18 @@ System.out.print("The value is one");
                 System.out.print("The value is not one");
                 break;
         }
+
+        Olive o1 = new Olive("Kalamata", 0x000000);
+        Olive o2 = new Olive("Picholine", 0x00FF00);
+        Olive o3 = new Olive("Ligurio", 0x000000);
+
+        Olive[] olives = {o1, o2, o3};
+        ArrayList<Olive> olives2=new AyyarList<>();
+        olives2.add(o1);
+        olives2.add(o2);
+        olives2.add(o3);
+
+        System.out.println(olives2);
 
     }
 }
